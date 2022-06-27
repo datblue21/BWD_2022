2de25b1c137e261e9fbe69ffe4045cf14be5272d
datblue21 <92386475+datblue21@users.noreply.github.com>
2022-06-27 20:07:20 +0700

    Push 27/06/22
    ok

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+// void f(int *m, int *n )
+// {
+//     (*m)++;
+//     (*n)++;
+// }
+int main() {
+    // Write C++ code here
+    cout << "Hello world!";
+    int *x, *y;
+    *x = 5; 
+    *y = 7;
+    // f(x,y);
+    cout << " x = " << *x << "; y = " << *y << endl;
+    return 0;
+}
